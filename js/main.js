1fc9591f280148e87d38792384e960d3600ec65c
Pirundy <pierce140he@gmail.com>
2022-09-29 14:49:52 -0500

    Added electron Json storage

--- js/main.js
+++ js/main.js
@@ -1,5 +1,8 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const os = require('os');
+//JSON Storage for electron
+const storage = require('electron-json-storage');
 const path = require('path')
 
 function createWindow () {
