f8bc7fa446bde0bde7c4fffece821757e38cdf45
Pirundy <pierce140he@gmail.com>
2022-10-27 14:54:16 -0500

    Revert "Squashed commit of the following:"
    This reverts commit 3edd2786a9625654ad5acb7fdd8b668188d9bdaf.

--- js/main.js
+++ js/main.js
@@ -60,21 +60,17 @@ ipcMain.handle("getAllAssignments", async (event) => {
   return result;
 });
 
-// ipc of getAssignmentById for renderer
-ipcMain.handle("getAssignmentById", async (event, id) => {
-  const result = getAssignmentById(id);
-  return result;
-});
+//  gets all the assignments that exist in the Assignments json
+function getAllAssignments() {
+  try {
+    let assns = storage.getSync("Assignments");
+    //stringify to make readable
+    return assns;
+  } catch (error) {
+    return "";
+  }
+}
 
-// ipc of editassignment for renderer
-ipcMain.handle("editAssignment", async (event, obj) =>{
-  const result = editAssignment(obj);
-  return result;
-});
-
-
-// Javascript regular functions
-// create assignment with input assignment object
 function createAssignment(assignment) {
   let key = "Assignments";
 
@@ -84,97 +80,17 @@ function createAssignment(assignment) {
     if (data != null && data.length > 0) {
       //set the new assignment id to the last assignment id + 1
       assignment.id = data[data.length - 1].id + 1;
-      //push assignment onto the data array
       data.push(assignment);
-      // replace the old data with the new data
       storage.set(key, data, function (error) {
         if (error) throw error;
       });
     } else {
       //if no assignments are in the array set the new assignment id to 0
       assignment.id = 0;
-      // replace the old data with the new data
       storage.set(key, [assignment], function (error) {
         if (error) throw error;
       });
     }
   });
   return true;
-}
-
-//  gets all the assignments that exist in the Assignments json
-function getAllAssignments() {
-  try {
-    //get assignments and return assignment array
-    let assns = storage.getSync("Assignments");
-    return assns;
-  } catch (error) {
-    //return empty string if no assignments
-    return "";
-  }
-}
-
-// function to get assignment using the id of the assignment
-function getAssignmentById(id) {
-  //grab the json information from the json with the key
-  let tempObj = storage.getSync(key);
-  //loop over all the items in the json array
-  for (let i = 0; i < tempObj.length; i++) {
-    //check to see if item in array is what we are looking for
-    if (tempObj.id[i] == id) {
-      //return the json object in the json array with id = to input id
-      return tempObj[i];
-    }
-  }
-
-  //return null if not able to find the assignment
-  return null;
-}
-
-// edit assignment name by the id of the assignment
-function setAssignmentName(id, name) {
-  let key = "Assignments";
-  // grab the assignments to edit
-  storage.get(key, function(error, data){
-    // iterate throughout the assignments array
-    for (let i = 0; i < assignments.length; i++) {
-      if (data[i].id == id) {
-        data[i].name = name;
-      }
-    }
-    //set the new data set with the new assignment name for the assignment by id
-    storage.set(key, data, function (error) {
-      if (error) throw error;
-    });
-  });
-  
-}
-
-// edit assignments by passing in the object to replace from the json array
-function editAssignment(obj){
-  let key = "Assignments";
-
-  //get all of the assignments
-  storage.get(key, function(error, data){
-    //check for no data
-    if(data != null && data.length > 0){
-      //search through all of the data 
-      for(let i = 0; i<data.length; i++){
-        //look for specific id
-        if(data[i].id == obj.id){
-          //set data index to object
-          data[i] = obj;
-          //replace the old data with the new data
-          storage.set("Assignments", data, function(error){
-            if (error) throw error;
-          });
-          //return the object that was stored
-          return obj;
-        }
-      }
-    }
-    //not able to get the assignment
-    return null;
-  });
-
 }
