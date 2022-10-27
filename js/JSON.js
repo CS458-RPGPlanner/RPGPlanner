dae2a2ae2fedeb404210602e68422d552cf85291
Pirundy <pierce140he@gmail.com>
2022-10-27 14:44:55 -0500

    RP-107 bugfix for invokes

--- js/JSON.js
+++ js/JSON.js
@@ -12,13 +12,12 @@ const { ipcRenderer } = require("electron");
 //       storage.set(key, data, function (error) {
 //         if (error) throw error;
 //       });
-//     } 
+//     }
 //   });
 // }
 // deleteAssignment(4);
 
 // Renderer process
-
 
 function createTask(key, object) {
   //check to make sure task doesn't already exist returns null obj if doesn't exist
@@ -48,21 +47,21 @@ taskObj = [{ name: "Aquire land", date: "2/3/1306" }];
 //html function to create an assignment based off the values in html assignment
 function createAssignment(obj) {
   ipcRenderer.invoke("createAssignment", obj).then((result) => {
-    alert(result);
+    return result;
   });
 }
 
 //html javascript function to get all assignments
 function getAssignments() {
   ipcRenderer.invoke("getAllAssignments").then((result) => {
-    alert(JSON.stringify(result));
+    return result;
   });
 }
 
 //html function to get an assignment by the id
 function getAssignmentById(id) {
-  ipcRenderer.invoke("getAssignmentById",id).then((result) => {
-    alert(JSON.stringify(result));
+  ipcRenderer.invoke("getAssignmentById", id).then((result) => {
+    return result;
   });
 }
 
@@ -74,40 +73,40 @@ function openForm() {
 // clears data from create assignment form after submit or cancel
 function closeForm() {
   document.getElementById("myForm").style.display = "none";
-	document.getElementsByName('points')[0].value = "";
-	document.getElementsByName('name')[0].value = "";
-	document.getElementsByName('date')[0].value = "";
-	document.getElementsByName('description')[0].value = "";
+  document.getElementsByName("points")[0].value = "";
+  document.getElementsByName("name")[0].value = "";
+  document.getElementsByName("date")[0].value = "";
+  document.getElementsByName("description")[0].value = "";
 }
 
 // submits assignment data and calls createAssignment function
 function saveAssignment() {
   // declare assignment fields for html form
-	var points = document.getElementsByName('points')[0].value;
-	var name = document.getElementsByName('name')[0].value;
-	var date = document.getElementsByName('date')[0].value;
-	var description = document.getElementsByName('description')[0].value;
+  var points = document.getElementsByName("points")[0].value;
+  var name = document.getElementsByName("name")[0].value;
+  var date = document.getElementsByName("date")[0].value;
+  var description = document.getElementsByName("description")[0].value;
 
   // validation checks to see if fields have data
-	if (points == null || points == "") {
-		alert("Points can't be blank");
-		return false;
-	} 
-	if (name == null || name == "") {
-		alert("Name can't be blank");
-		return false;
-	}
-	if (date == null || date == "") {
-		alert("Date can't be blank");
-		return false;
-	} 
-	if (description == null || description == "") {
-		alert("Description can't be blank");
-		return false;
-	} 
+  if (points == null || points == "") {
+    alert("Points can't be blank");
+    return false;
+  }
+  if (name == null || name == "") {
+    alert("Name can't be blank");
+    return false;
+  }
+  if (date == null || date == "") {
+    alert("Date can't be blank");
+    return false;
+  }
+  if (description == null || description == "") {
+    alert("Description can't be blank");
+    return false;
+  }
 
   // new assignment to be created in the json
-	newAssignment = {
+  newAssignment = {
     points,
     date,
     name,
@@ -115,9 +114,8 @@ function saveAssignment() {
   };
 
   // create the actual assignment with the ipc
-	createAssignment(newAssignment);
+  createAssignment(newAssignment);
 
   // close the form of the new assignment
-	closeForm();
+  closeForm();
 }
-
