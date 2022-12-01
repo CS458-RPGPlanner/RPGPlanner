0fea01e80afa301053f0b4a94bd4c1b3d78dd5b9
Maxwell1455 <Lamppam5393@my.uwstout.edu>
2022-12-01 12:44:32 -0600

    Squashed commit of the following:
    commit b95762ec38be26331e0467e7b95622b04e0a838b
    Author: meyert8034 <meyert8034@my.uwstout.edu>
    Date:   Tue Nov 29 12:20:30 2022 -0600
    
        Fixed assignment list due date
    
        Fixed an error that made the assignment list read "assignments.date" instead of "assignments[i].date"
    
    commit 035788df2bd7572bf2bc8c17cacac1f6bfeba28f
    Merge: 213200b 2a4fdcf
    Author: meyert8034 <60476530+meyert8034@users.noreply.github.com>
    Date:   Sun Nov 20 10:44:42 2022 -0600
    
        Merge pull request #31 from CS458-RPGPlanner/RP-123-Sprint-2-CSS
    
        Rp 123 sprint 2 css
    
    commit 2a4fdcf77d44afd859ffc4236aa81daf30b8bf33
    Merge: aed99b8 fbd03fc
    Author: FawnGuardian <thielea1319@my.uwstout.edu>
    Date:   Fri Nov 18 12:08:30 2022 -0600
    
        Merge branch 'rp-123-CreateAssnAndPageCSS' into RP-123-Sprint-2-CSS
    
    commit fbd03fc02b102f66cf10a298a37f2e19fa5859ca
    Author: FawnGuardian <thielea1319@my.uwstout.edu>
    Date:   Fri Nov 18 12:06:54 2022 -0600
    
        Task List CSS
    
        rp-123 Tasks listed in the assn list are indented in and match similar css as in the storyboard.
    
    commit aed99b81c74ad321ab6afa063e391180210af0fb
    Author: chancec5950 <60434788+chancec5950@users.noreply.github.com>
    Date:   Thu Nov 17 15:19:47 2022 -0600
    
        CSS tweaks to tabs and tasks
    
    commit 213200b90b7174c5965b81208837904ac608f80b
    Merge: 3488753 148f066
    Author: FawnGuardian <thielea1319@my.uwstout.edu>
    Date:   Thu Nov 17 14:32:11 2022 -0600
    
        Merge pull request #30 from CS458-RPGPlanner/RP-123-Sprint-2-CSS
    
        Rp 123 sprint 2 css
    
    commit 148f066ee72b6892753fcbf37563b1edc430d7d0
    Merge: 7e2d30c 3488753
    Author: FawnGuardian <thielea1319@my.uwstout.edu>
    Date:   Thu Nov 17 14:28:49 2022 -0600
    
        Merge branch 'main' into RP-123-Sprint-2-CSS
    
    commit 34887531e4174484cbf6bd56f372da3fcbbdc016
    Author: Pirundy <pierce140he@gmail.com>
    Date:   Thu Nov 17 14:27:44 2022 -0600
    
        bugfix for wrong get
    
    commit 7e2d30ca974e206e833455a60451dfacd90d6cdd
    Merge: 3ebb78d 82583a2
    Author: FawnGuardian <thielea1319@my.uwstout.edu>
    Date:   Thu Nov 17 14:19:05 2022 -0600
    
        Merge branch 'main' into RP-123-Sprint-2-CSS
    
    commit 3ebb78d7de3aa5d8b7c793d4164e8ee65df08df3
    Merge: 19794d2 686badc
    Author: chancec5950 <60434788+chancec5950@users.noreply.github.com>
    Date:   Thu Nov 17 14:11:48 2022 -0600
    
        Merge branch 'RP-123-Sprint-2-CSS' of https://github.com/CS458-RPGPlanner/RPGPlanner into RP-123-Sprint-2-CSS
    
    commit 19794d27eb44e3a4c52be1d966d1c38cb2d05ea7
    Author: chancec5950 <60434788+chancec5950@users.noreply.github.com>
    Date:   Thu Nov 17 14:11:09 2022 -0600
    
        RP-123 Reorganize CSS
    
    commit 82583a21fe407ba5a0f814b9c4e1d74d45c7c37d
    Author: Pirundy <pierce140he@gmail.com>
    Date:   Thu Nov 17 13:53:00 2022 -0600
    
        fixed the scaling for 1.25, 1.5, 1.75

--- js/JSON.js
+++ js/JSON.js
@@ -185,7 +185,7 @@ async function displayAssignments() {
     }
     dueTasks.innerHTML =
       "Due Date: " +
-      assignments.date +
+      assignments[i].date +
       "&emsp;Tasks: " +
       taskCounter +
       "/" +
@@ -231,7 +231,7 @@ async function displayAssignments() {
 
   let createButton = document.createElement("button");
   createButton.setAttribute("id", "createAssignmentList");
-  createButton.setAttribute("class", "defaultBtn");
+  createButton.setAttribute("class", "defaultBtn add");
   createButton.setAttribute("onclick", "openForm()");
 
   let createName = document.createElement("p");
@@ -330,7 +330,7 @@ async function displayTasks() {
 
     let arrow = document.getElementById("arrow-" + tasks[i].assignmentId);
     arrow.setAttribute("class", "fa fa-chevron-up");
-    
+
     let taskHeader = document.createElement("div");
     taskHeader.setAttribute("id", "taskHeader-" + tasks[i].id);
     taskHeader.setAttribute("class", "card-header");
@@ -379,12 +379,12 @@ async function displayTasks() {
 
       let createButton = document.createElement("button");
       createButton.setAttribute("id", "createAssignmentList");
-      createButton.setAttribute("class", "defaultBtn");
+      createButton.setAttribute("class", "defaultBtn add");
 
       let createName = document.createElement("p");
       createName.setAttribute("class", "createPlus");
       createName.innerHTML = "+";
-      
+
       createButton.append(createName);
 
       createHeader.append(createButton);
